0394cfdb9c96565d6720e53ac9bd65689adebb1a
sciiphoz <djiproduccer@gmail.com>
2025-02-24 13:00:17 +0300

    main page added

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -11,7 +11,51 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomAppBar(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: TextField(
+                style: TextStyle(color: Colors.blueGrey[600]),
+                cursorColor: Colors.white,
+                decoration: InputDecoration(
+                  filled: true,
+                  prefixIcon: Icon(Icons.search, color: Colors.blueGrey[600]),
+                  labelText: 'Поиск',
+                  labelStyle: TextStyle(color: Colors.blueGrey[600]),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(6),
+                    borderSide: BorderSide(color: Colors.white)
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(6),
+                    borderSide: BorderSide(color: Colors.white)
+                  )
+                )
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.05,
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: Text(
+                "Плейлисты",
+                style: TextStyle(fontSize: 42),
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.05,
+            ),
+            Scrollbar(
+              child: AboutDialog()
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(        
         child: Row
         (
           children: [
